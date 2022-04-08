049a9caffc9c5a3ddb83ba5323503d0e3fb0cbfa
asafbadiri <101919178+asafbadiri@users.noreply.github.com>
2022-04-08 05:25:34 +0300

    Update hexstr.py

--- utils/hexstr.py
+++ utils/hexstr.py
@@ -4,14 +4,14 @@ def hexstr(n):
     return "{:04x}".format(n)
 
 def main():
-    hexString = "No Input Number -\nUsege : python3 hexstr.py 4435"
+    hexString = "No Input Number -\nUsege : python3 hexstr.py 9435\nOutput: 24db"
     if len(sys.argv) > 1:
         args = sys.argv[1:]
         try:
             x = int(args[0])
             hexString = hexstr(x)
         except:
-            hexString = "Wrong Input - Please insert integer to translate into HEX value\nUsege : python3 hexstr.py 4435"
+            hexString = "Wrong Input - Please insert integer to translate into HEX value\nUsege : python3 hexstr.py 9435\nOutput: 24db"
     print(hexString)
     return 0
 

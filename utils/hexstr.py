1e53c01b43a02a8b4199626f5a092c382cc2c8ef
asafbadiri <101919178+asafbadiri@users.noreply.github.com>
2022-04-08 20:39:06 +0300

    Update hexstr.py

--- utils/hexstr.py
+++ utils/hexstr.py
@@ -1,28 +1,29 @@
-"""
 Program will translate integer to 4 chars Hex value
 Usege : python3 hexstr.py 9435
 Output: 24db
 """
 import sys
 
+print("Hello From hexstr")
+print("The value of __name__ is:", repr(__name__))
+
 def hexstr(n):
     return "{:04x}".format(n)
 
 def usage():
-    print("Wrong Input - Please insert integer to translate into HEX value\nUsege : python3 hexstr.py 9435\nOutput: 24db")
+    print("Wrong Input - Please insert integer to translate into HEX value\nUsege : python3 hexstr.py 9435 255 111\nOutput: 24db")
 
 def main():
-    if len(sys.argv) > 1:      # sys.argv[0] = python3
-        args = sys.argv[1:]    # get args list
+    if len(sys.argv) > 1:      # sys.argv = {"hexstr.py","9435","255","111"}
+        args = sys.argv[1:]    # args = {"9435"}
         try:
-            x = int(args[0])   # x = sys.argv[1]
-            print(hexstr(x))
+            for x in args:
+                print(hexstr(int(x)))
         except:
-            usage()            # wrong input fail to int() --> print usage()
+            usage()            # wrong input --> print usage()
     else:
-        usage()                # No input parameter --> print usage()
-    return 0
+        usage()                # wrong input --> print usage()
+    return 127
 
 if __name__ == "__main__":
     main()
-

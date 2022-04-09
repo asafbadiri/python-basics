628a441fb4e0f4f95a668ef3fe2231cbc45504c4
asafbadiri <101919178+asafbadiri@users.noreply.github.com>
2022-04-09 10:04:01 +0300

    Update main.py

--- tkinter/sqlite/main.py
+++ tkinter/sqlite/main.py
@@ -8,22 +8,18 @@ with sqlite3.connect("details.db") as db:
 cursor.execute("""CREATE TABLE IF NOT EXISTS users (id integer PRIMARY KEY AUTOINCREMENT, username text NOT NULL, password text NOT NULL);""")
 
 def add_new_user():
+    commited = False
     user_name = username.get()
     passwd = password1.get()
-    cursor.execute("SELECT COUNT(*) FROM users WHERE username ='" + user_name + "';")
-    result = cursor.fetchone()
-    commited = False
 
-    if int(result[0]) > 0:
-        error["text"] = "Error " + str(result[0]) + " USERS are using this username"
+    if find_user(user_name):
+        error["text"] = "Error another USER alredy exists"
     else:
         cursor.execute("INSERT INTO users(username,password) VALUES (?,?);",(user_name,passwd))
         db.commit()
         commited = True
     if commited:
-        cursor.execute("SELECT COUNT(*) FROM users WHERE username ='" + user_name + "';")
-        result = cursor.fetchone()
-        if int(result[0]) > 0:
+        if find_user(user_name):
             error["text"] = user_name + " inseted Successfully to DataBase"
         else:
             error["text"] = "Error when try to insert" + user_name + " to DataBase"
@@ -34,7 +30,7 @@ def find_my_password():
     cursor.execute("SELECT password FROM users WHERE username ='" + user_name + "';")
     result = cursor.fetchone()
     if str(result) == "None":
-        error["text"] = "None"
+        old_password.insert(0, "None")
     else:
         old_password.insert(0, result[0])
         #old_password.config(text = result[0]) #["text"] = result[0]
@@ -49,12 +45,11 @@ def show_all():
         tree.insert("", tk.END, values=row)    
 
 def change_password():
+    commited = False
     user_name = serchName.get()
-    new_password = old_password.get();
-    cursor.execute("SELECT COUNT(*) FROM users WHERE username ='" + user_name + "';")
-    result = cursor.fetchone()
+    new_password = old_password.get()
 
-    if int(result[0]) == 0:
+    if not find_user(user_name):
         error["text"] = "Error cannot find this username"
     else:
         cursor.execute("UPDATE users SET password = ? WHERE username = ?;",(new_password,user_name))
@@ -68,6 +63,31 @@ def change_password():
         else:
             error["text"] = "Password changed to " + result[0]
 
+def delete_user():
+    commited = False
+    user_name = serchName.get()
+
+    if not find_user(user_name):
+        error["text"] = "Error cannot find this username"
+    else:
+        cursor.execute("DELETE FROM users WHERE username ='" + user_name + "';")
+        db.commit()
+        commited = True
+    if commited:
+        if not find_user(user_name):
+            error["text"] = "User Deleted successfully"
+        else:
+            error["text"] = "Delete failed - Please connect Admin"
+    
+def find_user(user_name):
+    found = False
+    cursor.execute("SELECT COUNT(*) FROM users WHERE username ='" + user_name + "';")
+    result = cursor.fetchone()
+    if int(result[0]) > 0:
+        found = True
+    return found
+    
+    
 
 window = tk.Tk()
 window.geometry("650x680")
@@ -103,16 +123,19 @@ serchName = tk.Entry(text = "")
 serchName.place(x=150,y=180,width=100,height=25)
 
 old_password = tk.Entry(text = "")
-old_password.place(x=280,y=180,width=200,height=25)
+old_password.place(x=280,y=180,width=100,height=25)
 
-button2 = tk.Button(text= "FIND Password",command=find_my_password)
+button2 = tk.Button(text= "Show_User",command=find_my_password)
 button2.place(x=150,y=220,width=105,height=35)
 
-button3 = tk.Button(text= "Change Password",command=change_password)
+button3 = tk.Button(text= "Change_Pass",command=change_password)
 button3.place(x=300,y=220,width=105,height=35)
 
-button4 = tk.Button(text= "Show All",command=show_all)
-button4.place(x=250,y=270,width=105,height=35)
+button4 = tk.Button(text= "Delete",command=delete_user)
+button4.place(x=450,y=220,width=105,height=35)
+
+button5 = tk.Button(text= "Show_All",command=show_all)
+button5.place(x=250,y=270,width=105,height=35)
 
 tree = ttk.Treeview(window,column=("c1", "c2", "c3"), show='headings')
 tree.column("#1")

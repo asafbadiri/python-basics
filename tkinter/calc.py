7af226aaaf0399c71d411df53a4394fe8141bf4d
asafbadiri <101919178+asafbadiri@users.noreply.github.com>
2022-04-14 16:21:01 +0300

    Update calc.py

--- tkinter/calc.py
+++ tkinter/calc.py
@@ -6,22 +6,22 @@ def cal_mult(a,b): return a * b
 def cal_div(a,b): return "Dont use Zero" if b == 0 else a/b
 
 def action(act):
+   txt = "Assaf"
    funcions_list = {'cal_sum':(cal_sum,"+"),'cal_sub':(cal_sub,"-"),'cal_mult':(cal_mult,"*"),'cal_div':(cal_div,"/")}
    try:
       sm = funcions_list[act][0](int(a.get()),int(b.get()))
       if isinstance(sm, float):
-            txt = "{0} {1} {2} = {3:.3}".format(a.get(),funcions_list[act][1],b.get(),sm)
+          my_text_var.set("{0} {1} {2} = {3:.3}".format(a.get(),funcions_list[act][1],b.get(),sm))
       else:
-            txt = "{0} {1} {2} = {3}".format(a.get(),funcions_list[act][1],b.get(),sm)     
+          my_text_var.set("{0} {1} {2} = {3}".format(a.get(),funcions_list[act][1],b.get(),sm))
    except ValueError:
-         txt = "Wrong inputs"
-   label.config(text=txt)
+      my_text_var.set("Wrong inputs")
 
 win=Tk()
 win.title("Calculater TKinter");
 win.geometry("300x165")
 #window.iconbitmap('@c.xbm')  #Linux Only
-
+my_text_var = StringVar(value = "Hello! Please Insert Values")
 Label(win, text="Enter First Number", font=('Calibri 10')).grid(row=1, column= 1,columnspan=4)
 a=Entry(win, width=35)
 a.grid(row=2, column= 1,columnspan=4)
@@ -29,7 +29,7 @@ Label(win, text="Enter Second Number", font=('Calibri 10')).grid(row=3, column= 
 b=Entry(win, width=35)
 b.grid(row=4, column= 1,columnspan=4)
 
-label=Label(win, text="Total: ", font=('Calibri 15'))
+label=Label(win, textvariable = my_text_var, font=('Calibri 15'))
 label.grid(row=5, column= 1, columnspan=4, padx=20)
 
 Button(win, text="ADD", command= lambda: action("cal_sum")).grid(row=6, column= 1)

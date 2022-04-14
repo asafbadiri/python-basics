a91cb8657333beaef94de1c42527285a023cf7c7
asafbadiri <101919178+asafbadiri@users.noreply.github.com>
2022-04-14 15:21:30 +0300

    Update calc.py

--- tkinter/calc.py
+++ tkinter/calc.py
@@ -19,24 +19,17 @@ def cal_div(a,b):
 
 def action(act):
    try:
-      t1=int(a.get())
-      t2=int(b.get())
-      match act:
-         case "ADD":
-            sm = cal_sum(t1,t2)
-            txt = "{0} + {1} = {2}".format(t1,t2,sm)
-         case "SUB":
-            sm = cal_sub(t1,t2)
-            txt = "{0} - {1} = {2}".format(t1,t2,sm)
-         case "MULT":
-            sm = cal_mult(t1,t2)
-            txt = "{0} * {1} = {2}".format(t1,t2,sm)
-         case "DIV":
-            div = cal_div(t1,t2)
-            if isinstance(div, str):
-               txt = "{0} / {1} = {2}".format(t1,t2,div)
-            else:
-               txt = "{0} / {1} = {2:.3}".format(t1,t2,div)
+      funcions_list = {
+                  'cal_sum':(cal_sum,"+"),
+                  'cal_sub':(cal_sub,"-"),
+                  'cal_mult':(cal_mult,"*"),
+                  'cal_div':(cal_div,"/"),
+            }
+      sm = funcions_list[act][0](int(a.get()),int(b.get()))
+      if isinstance(sm, float):
+            txt = "{0} {1} {2} = {3:.3}".format(a.get(),funcions_list[act][1],b.get(),sm)
+      else:
+            txt = "{0} {1} {2} = {3}".format(a.get(),funcions_list[act][1],b.get(),sm)     
    except ValueError:
       txt = "Wrong inputs"
    label.config(text=txt)
@@ -44,7 +37,7 @@ def action(act):
 Label(win, text="Enter First Number", font=('Calibri 10')).grid(row=1, column= 1,columnspan=4)
 a=Entry(win, width=35)
 a.grid(row=2, column= 1,columnspan=4)
-a.insert(0, "First number")
+#a.insert(0, "First number")
 Label(win, text="Enter Second Number", font=('Calibri 10')).grid(row=3, column= 1,columnspan=4)
 b=Entry(win, width=35)
 b.grid(row=4, column= 1,columnspan=4)
@@ -52,9 +45,9 @@ b.grid(row=4, column= 1,columnspan=4)
 label=Label(win, text="Total: ", font=('Calibri 15'))
 label.grid(row=5, column= 1, columnspan=4, padx=20)
 
-Button(win, text="ADD", command= lambda: action("ADD")).grid(row=6, column= 1)
-Button(win, text="SUB", command= lambda: action("SUB")).grid(row=6, column= 2)
-Button(win, text="MULT", command= lambda: action("MULT")).grid(row=6, column= 3)
-Button(win, text="DIV", command= lambda: action("DIV")).grid(row=6, column= 4)
+Button(win, text="ADD", command= lambda: action("cal_sum")).grid(row=6, column= 1)
+Button(win, text="SUB", command= lambda: action("cal_sub")).grid(row=6, column= 2)
+Button(win, text="MULT", command= lambda: action("cal_mult")).grid(row=6, column= 3)
+Button(win, text="DIV", command= lambda: action("cal_div")).grid(row=6, column= 4)
 
 win.mainloop()

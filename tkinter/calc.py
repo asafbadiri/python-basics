24506dfdd7674249dfa24aada7dd5ec42a326f16
asafbadiri <101919178+asafbadiri@users.noreply.github.com>
2022-04-14 16:23:28 +0300

    Update calc.py

--- tkinter/calc.py
+++ tkinter/calc.py
@@ -6,13 +6,12 @@ def cal_mult(a,b): return a * b
 def cal_div(a,b): return "Dont use Zero" if b == 0 else a/b
 
 def action(act):
-   txt = "Assaf"
    funcions_list = {'cal_sum':(cal_sum,"+"),'cal_sub':(cal_sub,"-"),'cal_mult':(cal_mult,"*"),'cal_div':(cal_div,"/")}
    try:
       sm = funcions_list[act][0](int(a.get()),int(b.get()))
       if isinstance(sm, float):
           my_text_var.set("{0} {1} {2} = {3:.3}".format(a.get(),funcions_list[act][1],b.get(),sm))
-      else:
+      else: 
           my_text_var.set("{0} {1} {2} = {3}".format(a.get(),funcions_list[act][1],b.get(),sm))
    except ValueError:
       my_text_var.set("Wrong inputs")
@@ -28,13 +27,10 @@ a.grid(row=2, column= 1,columnspan=4)
 Label(win, text="Enter Second Number", font=('Calibri 10')).grid(row=3, column= 1,columnspan=4)
 b=Entry(win, width=35)
 b.grid(row=4, column= 1,columnspan=4)
-
 label=Label(win, textvariable = my_text_var, font=('Calibri 15'))
 label.grid(row=5, column= 1, columnspan=4, padx=20)
-
 Button(win, text="ADD", command= lambda: action("cal_sum")).grid(row=6, column= 1)
 Button(win, text="SUB", command= lambda: action("cal_sub")).grid(row=6, column= 2)
 Button(win, text="MULT", command= lambda: action("cal_mult")).grid(row=6, column= 3)
 Button(win, text="DIV", command= lambda: action("cal_div")).grid(row=6, column= 4)
-
 win.mainloop()

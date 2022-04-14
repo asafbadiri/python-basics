0cba8df8c28b84f7f210f2b50f33cc165065694b
asafbadiri <101919178+asafbadiri@users.noreply.github.com>
2022-04-14 15:37:24 +0300

    Update calc.py

--- tkinter/calc.py
+++ tkinter/calc.py
@@ -5,39 +5,26 @@ win.title("Calculater TKinter");
 win.geometry("300x165")
 #window.iconbitmap('@c.xbm');
 
-def cal_sum(a,b):
-      return a + b
-
-def cal_sub(a,b):
-   return a - b
-
-def cal_mult(a,b):
-   return a * b
-
-def cal_div(a,b):
-   return "Dont use Zero" if b == 0 else a/b
+def cal_sum(a,b): return a + b
+def cal_sub(a,b): return a - b
+def cal_mult(a,b): return a * b
+def cal_div(a,b): return "Dont use Zero" if b == 0 else a/b
 
 def action(act):
+   funcions_list = {'cal_sum':(cal_sum,"+"),'cal_sub':(cal_sub,"-"),'cal_mult':(cal_mult,"*"),'cal_div':(cal_div,"/")}
    try:
-      funcions_list = {
-                  'cal_sum':(cal_sum,"+"),
-                  'cal_sub':(cal_sub,"-"),
-                  'cal_mult':(cal_mult,"*"),
-                  'cal_div':(cal_div,"/"),
-            }
       sm = funcions_list[act][0](int(a.get()),int(b.get()))
       if isinstance(sm, float):
             txt = "{0} {1} {2} = {3:.3}".format(a.get(),funcions_list[act][1],b.get(),sm)
       else:
             txt = "{0} {1} {2} = {3}".format(a.get(),funcions_list[act][1],b.get(),sm)     
    except ValueError:
-      txt = "Wrong inputs"
+         txt = "Wrong inputs"
    label.config(text=txt)
 
 Label(win, text="Enter First Number", font=('Calibri 10')).grid(row=1, column= 1,columnspan=4)
 a=Entry(win, width=35)
 a.grid(row=2, column= 1,columnspan=4)
-#a.insert(0, "First number")
 Label(win, text="Enter Second Number", font=('Calibri 10')).grid(row=3, column= 1,columnspan=4)
 b=Entry(win, width=35)
 b.grid(row=4, column= 1,columnspan=4)
